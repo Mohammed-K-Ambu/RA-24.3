71c4d56fa33de9c82d3e0f216131ff81e1fc7f7c
Mohammed-K-Ambu <mohammed.ambusaidi@gmail.com>
2024-09-02 20:08:44 +0400

    Master

---
+++ server.test.js
@@ -0,0 +1,18 @@
+const request = require('supertest');
+const server = require('./server'); // Adjust path as necessary
+
+afterAll(() => {
+  server.close();
+});
+
+describe('Hello Word Returns', () => {
+  it('should return 200 OK for /api route', async () => {
+    const res = await request(server).get('/');
+    expect(res.status).toBe(200);
+  });
+
+  it('should return Hello World', async () => {
+    const res = await request(server).get('/');
+    expect(res.text).toBe('Hello world!');
+  });
+});
