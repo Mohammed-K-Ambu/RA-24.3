82fa71fa28fdb9b52d584980b19d93cf5afc7f43
Mohammed-K-Ambu <mohammed.ambusaidi@gmail.com>
2024-09-02 21:45:25 +0400

    Master 9

--- index.js
+++ index.js
@@ -6,4 +6,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+// Catch-all handler for any other routes
+app.get('*', (req, res) => {
+  res.status(404).send('Page not found');
+});
+
 exports.app = functions.https.onRequest(app);
